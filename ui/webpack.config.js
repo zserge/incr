1fffd6d3a2bffa31372c0aabc5438e380113eb54
Serge A. Zaitsev <zaitsev.serge@gmail.com>
2016-02-28 23:35:16 +0800

    added web ui

---
+++ ui/webpack.config.js
@@ -0,0 +1,20 @@
+var path = require('path');
+var config = {
+  entry: path.resolve(__dirname, 'app/app.jsx'),
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel', // 'babel-loader' is also a legal name to reference
+      query: {
+        presets: ['react', 'es2015']
+      }
+    }]
+  }
+};
+
+module.exports = config;
